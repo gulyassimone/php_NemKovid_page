7841899298636806264e270eac0685cc5e998373
Gulyás Simone <gulyassimone@gmail.com>
2020-12-30 18:31:00 +0100

    can step months

--- index.php
+++ index.php
@@ -5,7 +5,15 @@ include(__DIR__ . '/app/lib/helper.php');
 
 
 $appointment_storage = new AppointmentStorage();
+$uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
 
+
+
+if(isset($_GET['month'])){
+    $actualDate = $_GET['month'];
+}else{
+    $actualDate = strtotime(date('Y-m-d'));
+}
 ?>
 
 <!doctype html>
@@ -29,16 +37,37 @@ include(__DIR__ . "/app/template/navbar.php");
 
 <h2> Időpontok: </h2>
 <div class="container">
-    <div class="row align-items-start">
-        <div class="col">
-            <ul id=announced_dates>
-                <?php foreach ($appointment_storage->findAllActualMonth((int)date('m')) as $appointment) : ?>
-                    <li class="<?= count($appointment['users'])<(int)$appointment['free'] ? "text-success" : "text-danger" ?>"><?= date("Y.m.d H:i", $appointment['time']) ?>
-                        <?= count($appointment['users']) ?>\<?= $appointment['free'] ?> szabad hely</li>
-                <?php endforeach ?>
-            </ul>
-        </div>
-    </div>
+    <table class="table">
+        <caption><?= date('F', $actualDate) ?></caption>
+        <thead>
+        <tr>
+            <th scope="col">Dátum</th>
+            <th scope="col">Időpont</th>
+            <th scope="col">Szabad Helyek</th>
+            <th scope="col">jelentkezett felhasználók</th>
+            <th scope="col">Jelentkezés</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($appointment_storage->findAllActualMonth(date('m', $actualDate)) as $appointment) : ?>
+            <tr class="<?= count($appointment['users']) < (int)$appointment['free'] ? "table-success" : "table-danger" ?>">
+                <td><?= date("Y.m.d", $appointment['time']) ?></td>
+                <td><?= date("H:i", $appointment['time']) ?></td>
+                <td><?= count($appointment['users']) ?>\<?= $appointment['free'] ?> szabad hely</td>
+                <td> <?php foreach ($appointment['users'] as $user) : ?>
+                    <?= $user ?>,
+                    <?php endforeach ?>
+                </td>
+                <td><button type="button" id="apply" class="btn btn-info">Jelentkezés</button></td>
+                </div>
+            </tr>
+        <?php endforeach ?>
+        </tbody>
+    </table>
+<div>
+    <a class="btn btn-dark" href="<?=$uri_parts[0]?>?month=<?=strtotime('-1 months', $actualDate)?>">Előző hónap</a>
+    <a class="btn btn-dark" href="<?=$uri_parts[0]?>?month=<?=strtotime('+1 months', $actualDate)?>">Következő hónap</a>
+</div>
 </div>
 <?php
 include(__DIR__ . "/app/template/footer.php");
